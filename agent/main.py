4569393056920632e5e77ae6aa5dbae2c8160857
xpakx <55586470+xpakx@users.noreply.github.com>
2026-04-08 21:25:48 +0200

    feat: load config
    load config from file and parse with msgspec

--- agent/main.py
+++ agent/main.py
@@ -1,5 +1,26 @@
+import msgspec
+
+
+class Config(msgspec.Struct, rename="camel"):
+    api_key: str
+    provider: str
+    model: str
+
+
+def load_config(filename: str) -> Config:
+    with open(filename, 'r') as file:
+        data = file.read()
+    try:
+        config = msgspec.json.decode(data, type=Config)
+        return config
+    except msgspec.DecodeError as e:
+        raise Exception(f"decode error: {e}")
+
+
 def main():
     print("Hello from val-ai!")
+    config = load_config("data/config.json")
+    print(config)
 
 
 if __name__ == "__main__":
