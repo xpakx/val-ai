caeb6aaef91928b56371167f9720d3c7f3b842f8
xpakx <55586470+xpakx@users.noreply.github.com>
2026-04-08 21:33:05 +0200

    feat: connect to api

--- agent/main.py
+++ agent/main.py
@@ -1,4 +1,7 @@
 import msgspec
+# TODO: openai uses pydantic, so we might want
+# to switch to direct calls
+import openai
 
 
 class Config(msgspec.Struct, rename="camel"):
@@ -17,10 +20,28 @@ def load_config(filename: str) -> Config:
         raise Exception(f"decode error: {e}")
 
 
+class Client:
+    def __init__(self, config: Config):
+        self.model = config.model
+        self.client = openai.OpenAI(
+                api_key=config.api_key,
+                base_url=config.provider
+        )
+
+    def ask(self, messages: list[dict[str, str]]):
+        response = self.client.chat.completions.create(
+                model=self.model,
+                messages=messages
+        )
+        content = response.choices[0].message.content
+        return content
+
+
 def main():
     print("Hello from val-ai!")
     config = load_config("data/config.json")
-    print(config)
+    client = Client(config)
+    print(client.ask([{"role": "user", "content": "Hello!"}]))
 
 
 if __name__ == "__main__":
