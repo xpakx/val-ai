e1b401b8832dc9c4d060a4100d4fc1dc04c17637
xpakx <55586470+xpakx@users.noreply.github.com>
2026-04-15 20:17:42 +0200

    feat: let user use string templates for prompts

--- agent/prompt.py
+++ agent/prompt.py
@@ -1,5 +1,6 @@
 from typing import Protocol, Any
 from agent.signals import Signal, Computed, effect, Effect
+from string import Template
 
 
 class PromptPart(Protocol):
@@ -61,3 +62,50 @@ class Prompt:
                 [sig]
         )
         self._effects.append(eff)
+
+
+class TemplatedPrompt(Prompt):
+    def __init__(
+            self, template: Template, defaults: None | dict[str, Any] = None):
+        self._template: Template = template
+        self._text = ""
+        self.parts: list[PromptPart] = []
+        self.dirty = True
+        self.template_dirty = True
+        self._prepare_defaults(defaults)
+        self.parent: None | PromptPart = None
+        self._content = ""
+        self._show = True
+        self._effects: list[Effect] = []
+
+    def generate(self) -> str:
+        if self.template_dirty:
+            self._text = self.update_template()
+        content = self._text if self._show else ''
+
+        for part in self.parts:
+            part_value = part.content()
+            if part_value:
+                content += part_value
+                content += '\n'
+        return content
+
+    def update_template(self) -> str:
+        data = self._defaults | self._values
+        text = self._template.substitute(**data)
+        self.template_dirty = False
+        return text
+
+    def _prepare_defaults(self, defaults: None | dict[str, Any]) -> None:
+        self._defaults = {}
+        self._values = {}
+        t = self._template
+        print(t.template)
+        placeholders = self._template.get_identifiers()
+        print(placeholders)
+        for placeholder in placeholders:
+            print(placeholder)
+            if placeholder in defaults:
+                self._defaults[placeholder] = defaults[placeholder]
+            else:
+                self._defaults[placeholder] = f"${placeholder}"
