65c46a4a6bd244542bf79a447fcabeb74ad1030c
xpakx <55586470+xpakx@users.noreply.github.com>
2026-04-20 20:03:12 +0200

    feat: do not enforce having all config defined in json

--- agent/config.py
+++ agent/config.py
@@ -3,13 +3,29 @@ import os
 from pathlib import Path
 
 
-class Config(msgspec.Struct, rename="camel"):
+class RawConfig(msgspec.Struct, rename="camel"):
+    api_key: str | None = None
+    provider: str | None = None
+    model: str | None = None
+
+
+class Config(msgspec.Struct):
     api_key: str
     provider: str
     model: str
 
 
+def finalize_config(cfg: RawConfig) -> Config:
+    return msgspec.convert(msgspec.structs.asdict(cfg), Config)
+
+
 def load_config(filename: str | Path | None) -> Config:
+    conf = check_files(filename)
+    # TODO: env variables
+    return finalize_config(conf)
+
+
+def check_files(filename: str | Path | None) -> RawConfig:
     if filename:
         filepath = Path(filename)
         return load_config_from_file(filepath)
@@ -19,21 +35,20 @@ def load_config(filename: str | Path | None) -> Config:
         return load_config_from_file(local_config)
 
     config_dir = get_xdg_config_location()
-    config_file = config_dir / 'val/config.json'
-    if local_config.is_file():
+    config_file = config_dir / 'config.json'
+    if config_file.is_file():
         return load_config_from_file(config_file)
+    return RawConfig()
 
-    raise Exception("Couldn't find config file")
 
-
-def load_config_from_file(filepath:  Path) -> Config:
+def load_config_from_file(filepath:  Path) -> RawConfig:
     if not filepath.is_file():
         raise FileNotFoundError()
     data = filepath.read_text()
     if not data:
         raise Exception("Couldn't read file")
     try:
-        config = msgspec.json.decode(data, type=Config)
+        config = msgspec.json.decode(data, type=RawConfig)
         return config
     except msgspec.DecodeError as e:
         raise Exception(f"decode error: {e}")
@@ -44,9 +59,8 @@ def get_xdg(var: str, default: str) -> Path:
     if path_str:
         path_str = Path(path_str)
     else:
-        home = os.environ.get('HOME', '')
-        path_str = Path(home) / default
-    path = path_str / 'subsplease'
+        path_str = Path.home() / default
+    path = path_str / 'val'
     return path
 
 
