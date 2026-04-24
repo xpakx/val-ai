11ed22a1055798893ba10e4c778620710345287b
xpakx <55586470+xpakx@users.noreply.github.com>
2026-04-24 22:21:26 +0200

    refactor: extract preparation of tools

--- agent/main.py
+++ agent/main.py
@@ -2,7 +2,7 @@ from agent.config import load_config
 from agent.client import Client, ChatMessage, ToolCall, Message, TextMessage
 from agent.toolgen import get_tool, ToolDefinition
 from agent.ui import UIProvider, CLIProvider
-from agent.tools import read_file, list_files, write_file
+from agent.tools import read_file, list_files, write_file, glob_files
 from agent.systemparts import current_time
 from agent.systemprompt import get_system_prompt_info, SystemPromptInformation
 from agent.client.backoff import fibonacci_backoff
@@ -135,17 +135,22 @@ class Chat:
         self.info_subprompt.add_part(part)
 
 
-def main():
-    print("Hello from VAL-ai!")
-    config = load_config("data/config.json")
-    client = Client(config, fibonacci_backoff)
-    chat = Chat(client, CLIProvider())
+def prepare_tools(chat: Chat) -> None:
     read_tool = get_tool(read_file)
     chat.add_tool(read_tool)
     list_tool = get_tool(list_files)
     chat.add_tool(list_tool)
     write_tool = get_tool(write_file)
     chat.add_tool(write_tool)
+    glob_tool = get_tool(glob_files)
+    chat.add_tool(glob_tool)
+
+
+def main():
+    config = load_config("data/config.json")
+    client = Client(config, fibonacci_backoff)
+    chat = Chat(client, CLIProvider())
+    prepare_tools(chat)
 
     time = get_system_prompt_info(current_time)
     chat.add_system_part(time)
