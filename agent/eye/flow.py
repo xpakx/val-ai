cb75403a253778b99a226edb63f49a64d995dc2f
xpakx <55586470+xpakx@users.noreply.github.com>
2026-06-28 21:10:39 +0200

    feat: add parallel steps for flow feature

--- agent/eye/flow.py
+++ agent/eye/flow.py
@@ -7,7 +7,7 @@ from inspect import iscoroutinefunction
 class FlowFeature(EyeService):
     def __init__(
             self, name: str,
-            flow_definition: list[Callable],
+            flow_definition: list[Callable | tuple[Callable, ...]],
             ):
         self.name = name
         self.flow_definition = flow_definition
@@ -16,13 +16,17 @@ class FlowFeature(EyeService):
         loop_count = 1
         while True:
             for step_group in self.flow_definition:
-                await self._execute_step(app, step_group)
+                if isinstance(step_group, tuple):
+                    tasks = [self._execute_step(app, s) for s in step_group]
+                    await asyncio.gather(*tasks)
+                else:
+                    await self._execute_step(app, step_group)
 
             await app.emit(f"{self.name}:loop_done", loop=loop_count)
             loop_count += 1
-            await asyncio.sleep(0.5)
+            await asyncio.sleep(2.0)
 
-    async def _execute_step(self, app: "Eye", step: Callable) -> None:
+    async def _execute_step(self, app: Eye, step: Callable) -> None:
         if iscoroutinefunction(step):
             await step(app)
         elif callable(step):
@@ -35,10 +39,18 @@ if __name__ == "__main__":
     def step1(app: Eye):
         print("step 1")
 
-    def step2(app: Eye):
+    async def step2(app: Eye):
         print("step 2")
+        await asyncio.sleep(1.5)
+        print("step 2 done")
+    async def step3(app: Eye):
+        print("step 3")
+        await asyncio.sleep(0.5)
+        print("step 3 done")
+    def step4(app: Eye):
+        print("step 4")
 
-    flow = [step1, step2]
+    flow = [step1, (step2, step3), step4]
     app = Eye()
     app.add_service(FlowFeature("test", flow))
 
