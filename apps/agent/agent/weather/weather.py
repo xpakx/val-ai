faf839bf7355ad5c568a2bfb5911bbc68d48a0e6
xpakx <55586470+xpakx@users.noreply.github.com>
2026-07-16 22:46:38 +0200

    feat(agent): load weather for city in weather tool

--- apps/agent/agent/weather/weather.py
+++ apps/agent/agent/weather/weather.py
@@ -16,6 +16,20 @@ class GeocodingResponse(msgspec.Struct):
     results: list[Location] | None = None
 
 
+class CurrentWeather(msgspec.Struct):
+    time: str
+    interval: int
+    temperature_2m: float
+    weather_code: int
+
+
+class WeatherResponse(msgspec.Struct):
+    latitude: float
+    longitude: float
+    generationtime_ms: float
+    current: CurrentWeather
+
+
 def fetch_city_data(query: str) -> GeocodingResponse:
     url = "http://geocoding-api.open-meteo.com/v1/search"
     data = {
@@ -32,6 +46,24 @@ def fetch_city_data(query: str) -> GeocodingResponse:
     return result
 
 
+def get_weather(lat: float, long: float) -> WeatherResponse:
+    url = "https://api.open-meteo.com/v1/forecast"
+    data = {
+        "latitude": lat,
+        "longitude": long,
+        "current": "temperature_2m,weather_code",
+        "timezone": "auto"
+    }
+    headers = {'User-Agent': 'WeatherTool/1.0'}
+
+    response = requests.get(url, params=data, headers=headers)
+    response.raise_for_status()
+    return msgspec.json.decode(response.text, type=WeatherResponse)
+
+
 if __name__ == "__main__":
     result = fetch_city_data("London")
     print(result)
+    city = result.results[0]
+    weather = get_weather(city.latitude, city.longitude)
+    print(weather)
